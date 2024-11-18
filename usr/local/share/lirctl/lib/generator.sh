49ae40089ef27a1eb5018cb1e3555cb03005061d
Seyed Pouria Mousavizadeh Tehrani <p.mousavizadeh@protonmail.com>
2024-11-19 00:48:45 +0330

    rtm: bugfix on export name

--- usr/local/share/lirctl/lib/generator.sh
+++ usr/local/share/lirctl/lib/generator.sh
@@ -1,61 +1,52 @@
 #!/bin/sh
 
 # Generate import prefix list from $1 AS with $2 as-set
-in_ds_v6_gen()
-{
-	bgpq3 -m 48 -A6l IMPORT_IPV6_FROM_AS$1 $2
+in_ds_v6_gen() {
+  bgpq3 -m 48 -A6l IMPORT_IPV6_FROM_AS$1 $2
 }
 
 # Generate import as path list from $1 AS with $2 as-set
-in_ds_aspath_gen()
-{
-	bgpq3 -Dl IMPORT_ASN_FROM_AS$1 -f $1 $2
+in_ds_aspath_gen() {
+  bgpq3 -Dl IMPORT_ASN_FROM_AS$1 -f $1 $2
 }
 
 # Generate import prefix list from $1 AS without downstream
-in_nods_v6_gen()
-{
-	bgpq3 -m 48 -A6l IMPORT_IPV6_FROM_AS$1 AS$1
+in_nods_v6_gen() {
+  bgpq3 -m 48 -A6l IMPORT_IPV6_FROM_AS$1 AS$1
 }
 
 # Generate import as path list from $1 AS with $2 as-set
-in_nods_aspath_gen()
-{
-	echo "no bgp as-path access-list IMPORT_ASN_FROM_AS$1"
-	echo "bgp as-path access-list IMPORT_ASN_FROM_AS$1 deny .*"
+in_nods_aspath_gen() {
+  echo "no bgp as-path access-list IMPORT_ASN_FROM_AS$1"
+  echo "bgp as-path access-list IMPORT_ASN_FROM_AS$1 deny .*"
 }
 
 # Generate export prefix list from my AS with downstream
-out_ds_v6_gen()
-{
-	bgpq3 -Dl EXPORT_IPV6_FROM_AS$MY_ASN -f $MY_ASN $MY_AS_SET
+out_ds_v6_gen() {
+  bgpq3 -Dl EXPORT_IPV6_FROM_AS$MY_ASN -f $MY_ASN $MY_AS_SET
 }
 
 # Generate import prefix list from my AS without downstream
-out_nods_v6_gen()
-{
-	bgpq3 -m 48 -A6l IMPORT_IPV6_FROM_AS$MY_ASN AS$MY_ASN
+out_nods_v6_gen() {
+  bgpq3 -m 48 -A6l IMPORT_IPV6_FROM_AS$MY_ASN AS$MY_ASN
 }
 
 # Generate prefix list that matches with any ipv6
-any_ipv6_gen()
-{
-	echo "
+any_ipv6_gen() {
+  echo "
 ipv6 prefix-list ANY_IPV6 description ALL IPv6 ranges
 ipv6 prefix-list ANY_IPV6 seq 10 permit any
 "
 }
 
 # Generate as path list that matches with any asn
-any_aspath_gen()
-{
-	echo "bgp as-path access-list ANY_ASN permit .*"
+any_aspath_gen() {
+  echo "bgp as-path access-list ANY_ASN permit .*"
 }
 
 # Generate bogon ASNs as path list
-bogon_aspath_get()
-{
-	echo "
+bogon_aspath_get() {
+  echo "
 bgp as-path access-list BOGON_ASN seq 5 deny _0_
 bgp as-path access-list BOGON_ASN seq 10 deny _23456_
 bgp as-path access-list BOGON_ASN seq 15 deny _6449[6-9]_
@@ -80,9 +71,8 @@ bgp as-path access-list BOGON_ASN seq 105 permit .*
 "
 }
 
-bogon_rev_aspath_get()
-{
-	echo "
+bogon_rev_aspath_get() {
+  echo "
 bgp as-path access-list BOGON_REV_ASN seq 5 permit _0_
 bgp as-path access-list BOGON_REV_ASN seq 10 permit _23456_
 bgp as-path access-list BOGON_REV_ASN seq 15 permit _6449[6-9]_
@@ -107,9 +97,8 @@ bgp as-path access-list BOGON_REV_ASN seq 105 deny .*
 "
 }
 
-bogon_prefixlist_v4_get()
-{
-	echo "
+bogon_prefixlist_v4_get() {
+  echo "
 ip prefix-list BOGON_v4 deny 0.0.0.0/8 le 32
 ip prefix-list BOGON_v4 deny 10.0.0.0/8 le 32
 ip prefix-list BOGON_v4 deny 100.64.0.0/10 le 32
@@ -127,9 +116,8 @@ ip prefix-list BOGON_v4 deny 240.0.0.0/4 le 32
 "
 }
 
-bogon_prefixlist_v6_get()
-{
-	echo "
+bogon_prefixlist_v6_get() {
+  echo "
 ipv6 prefix-list BOGON_v6 deny ::/8 le 128
 ipv6 prefix-list BOGON_v6 deny 100::/64 le 128
 ipv6 prefix-list BOGON_v6 deny 2001:2::/48 le 128
@@ -147,22 +135,19 @@ ipv6 prefix-list BOGON_v6 deny ff00::/8 le 128
 }
 
 # Generate import route-map from $1 based on being our upstream or not $2
-in_rtm_v6_gen()
-{
-	if [ "$2" = "True" ]
-	then
-		UPSTREAM_ASN_LINE="ANY_ASN"
-		UPSTREAM_IPV6_LINE="ANY_IPV6"
-	elif [ "$2" = "False" ]
-	then
-		UPSTREAM_ASN_LINE="IMPORT_ASN_FROM_AS$1"
-		UPSTREAM_IPV6_LINE="IMPORT_IPV6_FROM_AS$1"
-	else
-		echo "lirctl -> Error: Upstream values in peer $1 should be yes or no"
-		exit 1
-	fi
+in_rtm_v6_gen() {
+  if [ "$2" = "True" ]; then
+    UPSTREAM_ASN_LINE="ANY_ASN"
+    UPSTREAM_IPV6_LINE="ANY_IPV6"
+  elif [ "$2" = "False" ]; then
+    UPSTREAM_ASN_LINE="IMPORT_ASN_FROM_AS$1"
+    UPSTREAM_IPV6_LINE="IMPORT_IPV6_FROM_AS$1"
+  else
+    echo "lirctl -> Error: Upstream values in peer $1 should be yes or no"
+    exit 1
+  fi
 
-	echo "
+  echo "
 route-map IMPORT_RTMV6_FROM_AS$1 deny 1
  description Drop Invalid RPKI
  match rpki invalid
@@ -204,9 +189,8 @@ exit
 }
 
 # Generate export route-map to $1
-out_rtm_v6_gen()
-{
-	echo "
+out_rtm_v6_gen() {
+  echo "
 route-map EXPORT_RTMV6_TO_AS$1 deny 1
  description Drop Invalid RPKI
  match rpki invalid
@@ -224,7 +208,7 @@ exit
 !
 route-map EXPORT_RTMV6_TO_AS$1 permit 10
  description Export netwroks with valid RPKI
- match ipv6 address prefix-list EXPORT_IPV6_FROM_AS$1
+ match ipv6 address prefix-list EXPORT_IPV6_FROM_AS$MY_ASN
  match ipv6 address prefix-list EXPORT_IPV6_NETWORK
  match rpki valid
 exit
@@ -236,17 +220,14 @@ exit
 "
 }
 
-# Generate export ipv6 prefix list from my ASN based on DOWNSTREAM value ($1) 
-out_my_v6_gen()
-{
-	if [ "$1" = "True" ]
-	then
-		out_ds_v6_gen
-	elif [ "$1" = "False" ]
-	then
-		out_nods_v6_gen
-	else
-		echo "lirctl -> Error: You can set your downstream value to only yes or no"
-		exit 1
-	fi
+# Generate export ipv6 prefix list from my ASN based on DOWNSTREAM value ($1)
+out_my_v6_gen() {
+  if [ "$1" = "True" ]; then
+    out_ds_v6_gen
+  elif [ "$1" = "False" ]; then
+    out_nods_v6_gen
+  else
+    echo "lirctl -> Error: You can set your downstream value to only yes or no"
+    exit 1
+  fi
 }
