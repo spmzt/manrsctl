1b92141a737f1bed44423866249efa0a11c7e8b6
Seyed Pouria Mousavizadeh Tehrani <p.mousavizadeh@protonmail.com>
2024-12-03 00:29:34 +0330

    add invalid rtm

--- usr/local/share/lirctl/lib/rtm.sh
+++ usr/local/share/lirctl/lib/rtm.sh
@@ -1,5 +1,33 @@
 #!/bin/sh
 
+# Drop invalid
+# Note: This route-map must be called by other route-maps
+invalid_rtm_get() {
+  echo "route-map RTM_INVALID_DENY deny 1
+ description Drop Invalid RPKI
+ match rpki invalid
+exit
+!
+route-map RTM_INVALID_DENY deny 2
+ description Drop IPv6 Bogon Prefixlist
+ match ipv6 address prefix-list PFL_BOGON
+exit
+!
+route-map RTM_INVALID_DENY deny 3
+ description Drop IPv4 Bogon Prefixlist
+ match ip address prefix-list PFL_V4_BOGON
+exit
+!
+route-map RTM_INVALID_DENY deny 4
+ description Drop Bogon AS Path
+ match as-path BOGON_REV_ASN
+exit
+!
+route-map RTM_INVALID_DENY permit 99
+ description call by other route-maps, permit anything
+exit"
+}
+
 # Generate import route-map from $1 based on being our upstream or not $2
 in_rtm_get() {
   if [ "$2" = "True" ]; then
@@ -12,20 +40,14 @@ in_rtm_get() {
     echo "lirctl -> Error: Upstream values in peer $1 should be yes or no"
     exit 1
   fi
+  
+  invalid_rtm_get
+  echo
 
-  echo "route-map IMPORT_RTMV6_FROM_AS$1 deny 1
- description Drop Invalid RPKI
- match rpki invalid
-exit
-!
-route-map IMPORT_RTMV6_FROM_AS$1 deny 2
- description Drop Bogon Prefixlist
- match ipv6 address prefix-list BOGON_v6
-exit
-!
-route-map IMPORT_RTMV6_FROM_AS$1 deny 3
- description Drop Bogon AS Path
- match as-path BOGON_REV_ASN
+  echo "route-map IMPORT_RTMV6_FROM_AS$1 permit 1
+ call RTM_INVALID_DENY
+ description Drop Invalid Prefixes
+ on-match next
 exit
 !
 route-map IMPORT_RTMV6_FROM_AS$1 permit 10
@@ -54,9 +76,10 @@ exit"
 
 # Generate export route-map to $1
 out_rtm_get() {
-  echo "route-map EXPORT_RTMV6_TO_AS$1 deny 1
- description Drop Invalid RPKI
- match rpki invalid
+  echo "route-map EXPORT_RTMV6_TO_AS$1 permit 1
+ call RTM_INVALID_DENY
+ description Drop Invalid Prefixes
+ on-match next
 exit
 !
 route-map EXPORT_RTMV6_TO_AS$1 deny 2
