a623834201d5d1e7cca85e317c83093f131c2d10
Seyed Pouria Mousavizadeh Tehrani <p.mousavizadeh@protonmail.com>
2024-12-08 14:37:34 +0330

    remove function execution inside cfg.sh to manrsctl

--- usr/local/share/manrsctl/lib/cfg.sh
+++ usr/local/share/manrsctl/lib/cfg.sh
@@ -1,26 +1,5 @@
 #!/bin/sh
 
-# Find config file
-cfg_get() {
-	export MANRSCTL_CONF;
-	if [ -f $HOME/.config/manrsctl/manrsctl.yaml ]
-	then
-		export MANRSCTL_CONF="$(realpath $HOME/.config/manrsctl/manrsctl.yaml)"
-	elif [ -f /usr/local/etc/manrsctl/manrsctl.yaml ]
-	then
-		export MANRSCTL_CONF="$(realpath /usr/local/etc/manrsctl/manrsctl.yaml)"
-	elif [ -f /etc/manrsctl/manrsctl.yaml ]
-	then
-		export MANRSCTL_CONF="$(realpath /etc/manrsctl/manrsctl.yaml)"
-	else
-		print_error "(manrsctl) Can't find configuration file."
-		exit 1
-	fi
-}
-
-# Run to set its MANRSCTL_CONF variables
-cfg_get
-
 parse_yml() {
 	python3 -m shyaml $@ < $MANRSCTL_CONF
 }
